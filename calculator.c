a637cfd56b8031523a2068888edba060348bbbd4
Huemer Patrick <pat.huemer@gmail.com>
2018-09-20 09:44:27 +0200

    calculator

---
+++ calculator.c
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: <your class>
+ * ---------------------------------------------------------
+ * Exercise Number: 0
+ * Title:			Pocket Calculator
+ * Author:		Patrick Huemer
+ * ----------------------------------------------------------
+ * Description:
+ * <some brief description, what the program does: example:
+ * Calculates a pyramid of numbers, i.e., it multiplies a big
+ * integer by the number 2, its result by 3, etc. and then
+ * returns to the original value by subsequently dividing,
+ * again starting by 2, 3, etc.>
+ * ----------------------------------------------------------
+ */
+
+ #include <stdio.h>
+
+
+ int int main(int argc, char const *argv[]) {
+   printf("Choose one of the following operations\n", );
+   printf("Add (1)\n", );
+   printf("Subtract (2)\n", );
+   printf("Multiply (3)\n", );
+   printf("Divide (4)\n", );
+   printf("Stop  program (-1)\n", );
+   printf("Enter your choice\n", );
+   return 0;
+ }
