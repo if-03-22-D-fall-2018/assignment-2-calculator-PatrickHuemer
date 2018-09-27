e7fb5739788c32ab6d52a84e2077abc7eb53768c
Huemer Patrick <pat.huemer@gmail.com>
2018-09-27 11:43:03 +0200

    Calculator.c

--- calculator.c
+++ calculator.c
@@ -16,14 +16,66 @@
 
  #include <stdio.h>
 
+ double add(double operand1, double operand2){
+   double sum = operand1 + operand2;
+   return sum;
+ }
+ double substract(double operand1, double operand2){
+   double sum = operand1 - operand2;
+   return sum;
+ }
+ double multiply(double operand1, double operand2){
+   double sum = operand1 * operand2;
+   return sum;
+ }
+ double divide(double operand1, double operand2){
+   double sum = operand1/ operand2;
+   return sum;
+ }
 
- int int main(int argc, char const *argv[]) {
-   printf("Choose one of the following operations\n", );
-   printf("Add (1)\n", );
-   printf("Subtract (2)\n", );
-   printf("Multiply (3)\n", );
-   printf("Divide (4)\n", );
-   printf("Stop  program (-1)\n", );
-   printf("Enter your choice\n", );
+int main(int argc, char const *argv[]) {
+   int choice;
+   double operand1;
+   double operand2;
+   double summe;
+
+   while (choice != -1) {
+     printf("Choose one of the following operations\n");
+     printf("Add (1)\n");
+     printf("Subtract (2)\n");
+     printf("Multiply (3)\n");
+     printf("Divide (4)\n");
+     printf("Stop  program (-1)\n");
+     printf("Enter your choice\n");
+     scanf("%d",&choice );
+     if (choice != -1) {
+       printf("Please enter first operand\n");
+       scanf("%lf",&operand1 );
+       printf("Please enter second operand\n");
+       scanf("%lf",&operand2 );
+
+     }
+     if (choice == 1) {
+       summe = add(operand1, operand2);
+     }
+     else if (choice == 2) {
+       summe = substract(operand1, operand2);
+     }
+     else if (choice == 3) {
+       summe = multiply(operand1, operand2);
+     }
+     else if (choice == 4) {
+       summe = divide(operand1, operand2);
+     }
+     if (choice != -1) {
+       printf("%lf\n",summe );
+     }
+
+
+
+   }
+
+
+
    return 0;
  }
